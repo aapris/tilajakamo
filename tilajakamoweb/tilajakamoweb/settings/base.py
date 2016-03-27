3eee921c93c209ffb98bc2427c82042d580c7b0c
Aapo (78.47.110.109) Rista <aapo@rista.net>
2016-03-27 14:19:27 +0300

    Add 'django.middleware.locale.LocaleMiddleware'

--- tilajakamoweb/tilajakamoweb/settings/base.py
+++ tilajakamoweb/tilajakamoweb/settings/base.py
@@ -170,6 +170,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 
     'wagtail.wagtailcore.middleware.SiteMiddleware',
     'wagtail.wagtailredirects.middleware.RedirectMiddleware',
